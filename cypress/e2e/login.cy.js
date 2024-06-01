df9a20acce803485cc9bd7a032d8118007ab6cd3
iqbalzayn01 <iqbalzayyanabiyyu@gmail.com>
2024-06-01 14:00:39 +0700

    update: add ci action

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,10 +1,6 @@
 /**
  * - Login spec
  *   - should display login page correctly
- *   - should display alert when username is empty
- *   - should display alert when password is empty
- *   - should display alert when username and password are wrong
- *   - should display homepage when username and password are correct
  */
 
 describe('Login spec', () => {
