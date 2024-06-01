d94fb20366be712de41a9b64459d9e0c12057ed0
iqbalzayn01 <iqbalzayyanabiyyu@gmail.com>
2024-06-01 20:30:39 +0700

    update: 2 ci check pass

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,10 +4,24 @@
  */
 
 describe('Login spec', () => {
-  it('should display login page correctly', () => {
+  beforeEach(() => {
+    // Set up before each test
     cy.visit('http://localhost:5173/signin');
+  });
+
+  it('should display login page correctly', () => {
     cy.get('input[name="email"]').should('be.visible');
     cy.get('input[name="password"]').should('be.visible');
     cy.contains('button', 'Sign In').should('be.visible');
+  });
+  it('should display error messages for invalid email or password', () => {
+    // Enter an invalid email
+    cy.get('input[name="email"]').type('invalidemail@example.com');
+    // Enter an invalid password
+    cy.get('input[name="password"]').type('invalidpassword');
+    // Click the sign-in button
+    cy.contains('button', 'Sign In').click();
+    // Check for the error message
+    cy.contains('Invalid email or password').should('be.visible');
   });
 });
